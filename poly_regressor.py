69e17f9748c1da7a473d4d3ff56bbee9757d3c9e
M Affan Nazir <m.affann80@hotmail.com>
2024-03-26 18:14:13 -0600

    batch Polynomial Regressor

--- poly_regressor.py
+++ poly_regressor.py
@@ -6,42 +6,91 @@ class Polynomial_Regressor:
 
     def __init__(self, order):
         self.order = order
+        self.weightInitialized = False
+
+    def name(self, name):
+        self.name = name
 
     def _getFeatureVector(self,data = []):
         data = [data]
         poly = PolynomialFeatures(self.order)
         features = poly.fit_transform(data)
         return features[0]
-    
-    def setEpochs(self,epochs):
-        self.epochs = epochs
-    
-    def train(self, features,target):
-        self.features = len(features[0])
-        self.w = np.random.rand(self.features)*0.01
-        for i in range(self.epochs):
-            for j in range(len(features)):
-                print("Epoch: " + str(i) + "  |  Percentage Completed: " + str(int(j/len(features)*100)))
-                self._epoch(features[j],target[j])
-            print("\n")
 
+    def _convertToFeatureMatrix(self,x):
+        matrix = []
+        for i in x:
+            matrix.append(self._getFeatureVector(i))
+        return matrix
     
-    def _epoch(self,x,y):
-        predicted = self._predict(x)
-        cost = self._cost(x,y)
-        g = np.array(x)*(np.array(predicted-y))
-        self.w = self.w - ((1+abs(g))**(-1))*g
+    
+    def train(self, trainingData=(),validationData=(),epochs=0,batchSize=1, stepSize=0.01):
+        xTrain =  np.array(self._convertToFeatureMatrix(trainingData[0]))
+        xVal = np.array(self._convertToFeatureMatrix(validationData[0]))
+        yTrain = np.array(trainingData[1])
+        yVal = np.array(validationData[1])
+        self.batchSize = batchSize
+        self.features = len(xTrain[0])
+        if self.weightInitialized == False:
+            self.w = np.random.rand(self.features)*0.01
+            self.weightInitialized = True
+        for i in range(epochs):
+            for j in range(0,len(xTrain),batchSize):
+                batchX = xTrain[j:j+batchSize]
+                batchY = yTrain[j:j+batchSize]
+                self._epoch(batchX,batchY, stepSize)
+            loss = self._cost(xTrain,yTrain)
+            valLoss = self._valLoss(xVal,yVal)
+            print("Epoch " + str(i+1) + ": | loss = " + str(self._round(loss,3)) + " validation loss = " + str(self._round(valLoss,3)))
+        
+    
+    def _epoch(self,x,y, stepSize):
+        g = self._grad(x,y)
+        self.w = self.w - stepSize * g
+        pass
     
     def _cost(self, x, y):
         p = self._predict(x)
+        cost = 0
         #MSE:
-        return (p - y)**2
-        
+        for i in range(len(p)):
+            cost += (p[i] - y[i])**2
+        cost = cost/len(p)
+        return cost
     
+    def _grad(self, x, y):
+        p = self._predict(x)
+        err = p-y
+        grad = np.dot(x.T, (err / len(y)))
+        return grad
+    
+    def _valLoss(self, x,y):
+        return self._cost(x,y)
+
     def _predict(self, x):
         return np.dot(x,self.w)
+    
+    def _round(self, num, sig_figs):
+        if num == 0:
+            return 0
+        else:
+            return round(num, sig_figs - int(np.floor(np.log10(abs(num)))) - 1)
 
-        
+    def save(self, path=None):
+        if path is None:
+            path = os.getcwd()
+        if not os.path.isdir(path):
+            print(f"The directory {path} does not exist.")
+            return
+
+        file_path = os.path.join(path, self.name + ".prf")
+
+        w_str = str(self.w)
+        try:
+            with open(file_path, 'w') as f:
+                f.write(w_str)
+        except IOError as e:
+            print(f"An error occurred: {e}")
     
     
 
